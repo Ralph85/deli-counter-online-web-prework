079ea3cf91ee7649bc7ce54e44a684f48f90a551
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-12 05:16:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,5 @@
-# Write your code here.
+bibboz_deli = []
+
+  take_a_number(bibboz_deli, "James") #=> Welcome, James. You are number 1 in line.
+  take_a_number(bibboz_deli, "Kim") #=> Welcome, Kim. You are number 2 in line.
+  
